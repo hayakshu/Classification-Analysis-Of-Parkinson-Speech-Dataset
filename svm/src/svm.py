8c64783f5ffa804f8fdd1388943bf126332f2330
Shu <shusinthebox@gmail.com>
2015-12-11 10:55:37 -0500

    Script for svm on Test Set

---
+++ svm/src/svm.py
@@ -0,0 +1,66 @@
+from sklearn.svm import SVC
+import numpy as np
+import pandas as pd
+
+
+def svm(trainName, trainPatientTypeName, testName, testPatientTypeName,k="linear", g='auto'):
+	# Load data from files by converting to Pandas DataFrame
+	trainData = pd.read_csv(trainName)
+	trainPatientType = pd.read_csv(trainPatientTypeName)
+	testData = pd.read_csv(testName)
+	testPatientType = pd.read_csv(testPatientTypeName)
+
+	# Remove header and patient index numbers from Pandas DataFrame
+	# and convert to Numpy Array
+	trainData = trainData.iloc[:,1:]
+	trainData = np.array(trainData)
+	trainPatientType = trainPatientType.iloc[:,1:]
+	trainPatientType = np.array(trainPatientType).ravel()
+
+	testData = testData.iloc[:,1:]
+	testData = np.array(testData)
+	testPatientType = testPatientType.iloc[:,1:]
+	testPatientType = np.array(testPatientType).ravel()
+
+	print("Training Model...")
+	clf = SVC(C=1.0, kernel=k, gamma=g)
+	clf.fit(trainData, trainPatientType)
+	predictions = clf.predict(testData)
+	print("Done!")
+	print("Calculating Accuracy...")
+	TP, FN, TN, FP = 0, 0, 0, 0
+	for i, prediction in enumerate(predictions):
+		if(prediction == 1 and testPatientType[i] == 1):
+			TP += 1
+		elif(prediction == 0 and testPatientType[i] == 1):
+			FN += 1
+		elif(prediction == 0 and testPatientType[i] == 0):
+			TN += 1
+		elif(prediction == 1 and testPatientType[i] == 0):
+			FP += 1
+		else:
+			pass
+	# testPatientType
+	# predictions
+	Accuracy = (TP + TN)/float(TP + TN + FP + FN)
+	print("Accuracy of Prediction {}: {}".format(k.upper(),Accuracy))
+
+print("Predicting PCA Data")
+# File path for training set - preprocessed via PCA and not
+trainName = "../dataset/pca/train.csv"
+trainPatientTypeName = "../dataset/patient_type/train.csv"
+testName = "../dataset/pca/test.csv"
+testPatientTypeName = "../dataset/patient_type/test.csv"
+svm(trainName, trainPatientTypeName, testName, testPatientTypeName)
+svm(trainName, trainPatientTypeName, testName, testPatientTypeName,"rbf",1e-06)
+print("Done Predicting PCA Data\n")
+
+print("Predicting NON-PCA Data")
+trainName = "../dataset/pandas/train.csv"
+testName = "../dataset/pandas/test.csv"
+svm(trainName, trainPatientTypeName, testName, testPatientTypeName)
+svm(trainName, trainPatientTypeName, testName, testPatientTypeName,"rbf",1e-06)
+print("Done Predicting NON-PCA Data\n")
+
+
+
