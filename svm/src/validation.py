6a4ffea58c0fc98c72c00d55fa17c510d1fb9b70
Shu <shusinthebox@gmail.com>
2015-12-11 09:52:26 -0500

    Removed Validation Folder * Added Cross Validation script to src folder

--- svm/src/validation.py
+++ svm/src/validation.py
@@ -5,6 +5,10 @@ from numpy import loadtxt
 import numpy as np
 import pandas as pd
 
+# ---------------------------------------------- #
+# SCRIPT FOR CROSS VALIDATION ON SLOO DATASETS
+# ---------------------------------------------- #
+
 # Cross Validation Script on SLOO Data to determine 
 # Linear Kernal Cross Validation 
 def linear(X, Y):
@@ -66,3 +70,35 @@ def rbf(X, Y):
 	clf.fit(X, Y)
 	print(clf.best_params_)
 	print(clf.best_score_)
+
+# Cross Validation for pca
+# File path for training set - preprocessed via PCA and not
+trainName = "../dataset/pca_train.csv"
+trainPatientTypeName = "../dataset/train_patientType.csv"
+
+# Load data from files by converting to Pandas DataFrame
+trainData = pd.read_csv(trainName)
+trainPatientType = pd.read_csv(trainPatientTypeName)
+
+# Remove header and patient index numbers from Pandas DataFrame
+# and convert to Numpy Array
+trainData = trainData.iloc[:,1:]
+trainData = np.array(trainData)
+trainPatientType = trainPatientType.iloc[:,1:]
+trainPatientType = np.array(trainPatientType).ravel()
+
+linear(trainData, trainPatientType)
+rbf(trainData, trainPatientType)
+
+# Cross Validation on Non PCA Method
+trainName = "../dataset/train.csv"
+
+# Remove header and patient index numbers from Pandas DataFrame
+# and convert to Numpy Array
+trainData = trainData.iloc[:,1:]
+trainData = np.array(trainData)
+trainPatientType = trainPatientType.iloc[:,1:]
+trainPatientType = np.array(trainPatientType).ravel()
+
+linear(trainData, trainPatientType)
+rbf(trainData, trainPatientType)
