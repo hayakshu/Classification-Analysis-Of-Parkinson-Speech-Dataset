a07f01ea8b4981bf128afe5b2a0e1382b29915b0
Shu <shusinthebox@gmail.com>
2015-12-11 05:57:56 -0500

    Changes to utility file * removed script to load data from files * added script to extract patient type for each patient

--- svm/src/utility.py
+++ svm/src/utility.py
@@ -1,22 +1,43 @@
 import numpy as np
+import pandas as pd
+from numpy import loadtxt, unique
 
-# Fix so we split results from training set
-def load_training():
-	x_train = []
-	y_train = []
-	with open('../dataset/train_data.txt') as train:
-		for row in train:
-			tple = eval(row)
-			x_train.append(list(tple[:(len(tple)-1)]))
-			y_train.append(tple[(len(tple)-1):][0])
-	
-	return (np.array(x_train), np.array(y_train))
+# Script for extracting expected patient type into seperate array
 
-def load_test():
-	test_set = []
-	with open('../dataset/test_data.txt') as test:
-		for row in test:
-			tple = eval(row)
-			test_set.append(list(tple[:(len(tple))]))
-	
-	return np.array(test_set)
+# For extracting unqiue patient type from training and testing
+def patient_type(dataset, unique):
+	count = 0 # To keep track of patient number
+	data_results = []	
+	for i in unique:
+		prevPatient = None
+		for data in dataset:
+			if(i == data[0] and prevPatient != data[0]):
+				result = data[(dataset.shape[1]-1)]
+				data_results.append(result)
+				prevPatient = data[0]
+
+	return data_results
+
+trainName = "../dataset/train_data.txt"
+testName = "../dataset/test_data.txt"
+
+trainData = loadtxt(trainName, delimiter=",")
+testData = loadtxt(testName, delimiter=",")
+
+trainData = np.array(trainData)
+testData = np.array(testData)
+
+# Extracting number of patients
+n_patientsTrain = unique(trainData[:,0])
+n_patientsTest = unique(testData[:,0])
+
+# Extracting Data if each patient has Parkinsons or Not
+train_results = patient_type(trainData, n_patientsTrain)
+test_results = patient_type(testData, n_patientsTest)
+
+# Saving Results through Pandas DataFrame
+train_results = pd.DataFrame(train_results)
+test_results = pd.DataFrame(test_results)
+print("Saving Results Data to CSV File")
+train_results.to_csv("../dataset/train_patientType.csv")
+test_results.to_csv("../dataset/test_patientType.csv")
