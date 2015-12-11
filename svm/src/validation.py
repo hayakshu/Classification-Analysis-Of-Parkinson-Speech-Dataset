e19e7d422b3284ea0d76d07714b6f8045b2846c5
Shu <shusinthebox@gmail.com>
2015-12-11 10:04:56 -0500

    Removed unneccessary code

--- svm/src/validation.py
+++ svm/src/validation.py
@@ -92,13 +92,12 @@ rbf(trainData, trainPatientType)
 
 # Cross Validation on Non PCA Method
 trainName = "../dataset/pandas/train.csv"
+trainData = pd.read_csv(trainName)
 
 # Remove header and patient index numbers from Pandas DataFrame
 # and convert to Numpy Array
 trainData = trainData.iloc[:,1:]
 trainData = np.array(trainData)
-trainPatientType = trainPatientType.iloc[:,1:]
-trainPatientType = np.array(trainPatientType).ravel()
 
 linear(trainData, trainPatientType)
 rbf(trainData, trainPatientType)
