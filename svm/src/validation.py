6e6ec3671114b1abf11bcb6c7bc966c0dacda247
Shu <shusinthebox@gmail.com>
2015-12-13 12:22:12 -0500

    Added method to generate report on RBF method validation results

--- svm/src/validation.py
+++ svm/src/validation.py
@@ -1,5 +1,6 @@
 from sklearn.cross_validation import LeaveOneOut
 from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import matthews_corrcoef, classification_report, confusion_matrix
 from sklearn.svm import SVC
 from numpy import loadtxt
 import numpy as np
@@ -18,6 +19,8 @@ def linear(X, Y):
 	
 	loo = LeaveOneOut(dataLength)
 	avg_Accuracy = dict()
+	sensitivity = dict()
+	specificity = dict()
 	for c in C:
 		#print "Performing Cross Validation on Penalty: {}".format(c)
 		predictions = []
@@ -26,7 +29,7 @@ def linear(X, Y):
 		Accuracy = 0
 		for train_index, test_index in loo:
 			X_train, X_test = X[train_index], X[test_index]
-			Y_train, Y_test = Y[train_index], Y[test_index]
+			Y_train, Y_test = Y[train_index], Y[test_index][0]
 
 			clf = SVC(C=c, kernel='linear')
 			clf.fit(X_train, Y_train)
@@ -48,16 +51,28 @@ def linear(X, Y):
 				FP += 1
 			else:
 				pass
+		Sensitivity = TP/float(TP + FN)
+		Specificity = TN/float(TN + FP)
 		Accuracy = (TP + TN)/float(TP + TN + FP + FN)
 		#print("Accuracy of Prediction: {} @ Penalty: {}".format(Accuracy, c))
 		avg_Accuracy[c] = Accuracy
-
-	print(avg_Accuracy)
+		sensitivity[c] = Sensitivity
+		specificity[c] = Specificity
 
 	bestC = max(avg_Accuracy.iterkeys(), key=(lambda k: avg_Accuracy[k]))
+	bestSensitivity = sensitivity[bestC]
+	bestSpecificity = specificity[bestC]
 	bestAccuracy = avg_Accuracy[bestC]
+
 	print("Most Accurate Penalty Value: {}".format(bestC))
-	print("Accuracy: {}".format(bestAccuracy))
+	print("Accuracy of Prediction: {} @ Penalty: {}".format(bestAccuracy, c))
+	print("Sensitivity of Prediction: {} @ Penalty: {}".format(bestSensitivity, c))
+	print("Specificity of Prediction: {} @ Penalty: {}".format(bestSpecificity, c))
+	print("Matthews Correlation Coeefficient Value: {}".format(matthews_corrcoef(predictions, expected)))
+	print("Classification Report:")
+	print(classification_report(predictions, expected))
+	print("Confusion Matrix")
+	print(confusion_matrix(predictions, expected))
 
 def rbf(X, Y):
 	# Performing Grid Search for Parameter Selection
@@ -71,6 +86,59 @@ def rbf(X, Y):
 	clf.fit(X, Y)
 	print("Best Params for RBF: {}".format(clf.best_params_))
 	print("Accuracy: {}".format(clf.best_score_))
+	return clf.best_params_
+
+# Reports results for validation on RBF: calculates the Sensitiviy and Specificity of Best Hyperparameters from validation set
+def rbf_analysis(X, Y, c, g):
+
+	print "Performing Cross Validation on Penalty: {}".format(c)
+	dataLength = len(X)
+	loo = LeaveOneOut(dataLength)
+	predictions = []
+	expected = []
+	TP, FN, TN, FP = 0, 0, 0, 0
+	Accuracy = 0
+	for train_index, test_index in loo:
+		X_train, X_test = X[train_index], X[test_index]
+		Y_train, Y_test = Y[train_index], Y[test_index][0]
+
+		clf = SVC(C=c, gamma=g, kernel='rbf')
+		clf.fit(X_train, Y_train)
+		prediction = clf.predict(X_test)[0]
+	
+		predictions.append(prediction)
+		expected.append(Y_test)
+
+	print("Calculating.....")
+	for i, prediction in enumerate(predictions):
+		if(prediction == 1 and expected[i] == 1):
+			TP += 1
+		elif(prediction == 0 and expected[i] == 1):
+			FN += 1
+		elif(prediction == 0 and expected[i] == 0):
+			TN += 1
+		elif(prediction == 1 and expected[i] == 0):
+			FP += 1
+		else:
+			pass
+
+	Sensitivity = TP/float(TP + FN)
+	Specificity = TN/float(TN + FP)
+	Accuracy = (TP + TN)/float(TP + TN + FP + FN)
+
+	print("Sensitivity of Prediction: {} @ Penalty: {} @ Gamma: {}".format(Sensitivity, c, g))
+	print("Specificity of Prediction: {} @ Penalty: {} @ Gamma: {}".format(Specificity, c, g))
+	print("Accuracy of Prediction: {} @ Penalty: {} @ Gamma: {}".format(Accuracy, c, g))
+	print("Matthews Correlation Coeefficient Value: {}".format(matthews_corrcoef(predictions, expected)))
+	print("Classification Report:")
+	print(classification_report(predictions, expected))
+	print("Confusion Matrix")
+	print(confusion_matrix(predictions, expected))
+
+
+# ------------------------------------------ #
+# PCA DATA VALIDATION						 #
+# ------------------------------------------ #
 
 # Cross Validation for pca
 # File path for training set - preprocessed via PCA and not
@@ -88,10 +156,18 @@ trainData = trainData.iloc[:,1:]
 trainData = np.array(trainData)
 trainPatientType = trainPatientType.iloc[:,1:]
 trainPatientType = np.array(trainPatientType).ravel()
-
+print("\nPerforming Linear SVM Cross-Validation...")
 linear(trainData, trainPatientType)
-rbf(trainData, trainPatientType)
+print("\nPerforming RBF SVM Cross-Validation...")
+hyperparameters = rbf(trainData, trainPatientType)
+c = hyperparameters['C']
+gamma = hyperparameters['gamma']
+rbf_analysis(trainData, trainPatientType, c, gamma)
 
+
+# ------------------------------------------ #
+# NON-PCA DATA VALIDATION					 #
+# ------------------------------------------ #
 print("\nPerforming Cross Validation on regular SLOO Data")
 # Cross Validation on Non PCA Method
 trainName = "../dataset/pandas/train.csv"
@@ -102,5 +178,11 @@ trainData = pd.read_csv(trainName)
 trainData = trainData.iloc[:,1:]
 trainData = np.array(trainData)
 
+print("\nPerforming Linear SVM Cross-Validation...")
 linear(trainData, trainPatientType)
-rbf(trainData, trainPatientType)
+
+print("\nPerforming RBF SVM Cross-Validation...")
+hyperparameters = rbf(trainData, trainPatientType)
+c = hyperparameters['C']
+gamma = hyperparameters['gamma']
+rbf_analysis(trainData, trainPatientType, c, gamma)
